2a87a54c69b615e50daa82161838a73a5867a85f
Nathan Worden <nathanworden@Nathans-MacBook-Pro.local>
2021-05-03 19:46:58 -0700

    python points

---
+++ 01.chapter1_quiz.py
@@ -0,0 +1,16 @@
+# What are the six main reasons that people choose to use Python?
+# 1) Sofware quality
+# 2) Developer productivity
+# 3) Program portability
+# 4) Support Libraries
+# 5) Component integration
+# 6) Enjoyment
+
+# Name four notable companies or organizations using Python today.
+
+
+# Freedom oof expression is great for art buy lousy for engineering.
+
+# In engineering, we need a minimal feature set and predictability. In engineering, freedom of expression can lead to maintenance nightmares. As more than one Perl user has confided to me, the result of too much freedom is often code that is much easier to rewrite from scratch than to modify. This is clearly less than ideal.
+
+# Consider this: when people create a painting or a sculpture, they do so largely for themselves; the prospect of someone else changing their work later doesn’t enter into it. This is a critical difference between art and engineering. When people write soft- ware, they are not writing it for themselves. In fact, they are not even writing primarily for the computer. Rather, good programmers know that code is written for the next human being who has to read it in order to maintain or reuse it. If that person cannot understand the code, it’s all but useless in a realistic development scenario. In other words, programming is not about being clever and obscure—it’s about how clearly your program communicates its purpose.
---
+++ 02.how_python_runs_programs.py
@@ -0,0 +1,6 @@
+# An nterpreter is a kind of program that executes other programs.
+
+print('hello world')
+print(2 ** 100)
+
+# When you instruct Python to run your script, there are a few steps that Python carries out before your code actually starts crunching away. Specifically, it’s first compiled to something called “byte code” and then routed to something called a “virtual machine.”
