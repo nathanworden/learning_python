b363289117ec7f746a76487a7c165dfb164a407d
Nathan Worden <worden.nathan@gmail.com>
2021-05-05 21:55:59 -0700

    Experimenting

---
+++ 03.1_Running_code_interactively.py
@@ -0,0 +1,8 @@
+print("Hello world!")
+print(2 ** 8)
+
+# The interactie prompt is a great place to experiment with the language and test program files on the fly
+
+
+print('Spam!' * 8)
+print('*'*8)
--- 03.How_You_Run_Programs.py
+++ 03.How_You_Run_Programs.py
@@ -1,3 +1,7 @@
 # Starting an Interactive Session
 
 # Start an interactive interpreter session by typing `python` at your operating syste's prompt.
+
+# The System Path
+
+# The PATH setting lists a set of directoris that the operating system searches for executable programs, when they are invoked without a full directory path. It should normally include the directory where your Python interpreter lives. 
