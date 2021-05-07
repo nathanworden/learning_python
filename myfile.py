f6b4a1d2777888d7f50a0cc753894932965b7aab
Nathan Worden <worden.nathan@gmail.com>
2021-05-07 11:56:52 -0700

    import myfile into IDE

---
+++ myfile.py
@@ -0,0 +1,7 @@
+title = "The Meaning of Life"
+
+
+# The dot expression syntax object.attribute lets you fetch any attribute attached to any object, and is one of the most common operations in Python code.
+
+
+doge = "cat"
