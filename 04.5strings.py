90a1661a1b531195fc0b46dbca3549cc4788695f
Nathan Worden <worden.nathan@gmail.com>
2021-05-15 22:19:51 -0700

    immutable strings

--- 04.5strings.py
+++ 04.5strings.py
@@ -1,2 +1,16 @@
 S = 'Spam'
 print(S[1:3])
+
+# X[I:J], means "give me everything in  from offset I up to but not including offset J"
+
+print(S[1:])
+print(S)
+print(S[0:3])
+print(S[0:4])
+print(S[:-1])
+print(S[:])    # All of S as a top-level copy (0:len(S))
+
+print(S + 'xyz')
+print(S)
+print()
+print(S * 8)
---
+++ 04.6_immutable_strings.py
@@ -0,0 +1,1 @@
+# Strings are immutable in Python - they cannot be changed in place after they are created. You can never overwrite the values of immtable objects.
