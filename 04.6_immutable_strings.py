3ecbd35f64f302fe5e28bcfc848a060c5ce9fdf4
Nathan Worden <worden.nathan@gmail.com>
2021-05-16 21:51:50 -0700

    strings

--- 04.6_immutable_strings.py
+++ 04.6_immutable_strings.py
@@ -1,1 +1,32 @@
 # Strings are immutable in Python - they cannot be changed in place after they are created. You can never overwrite the values of immtable objects.
+
+S = 'Spam'
+
+# S[0] = 'z'
+
+
+# numbers, strings, and tuples are immutable
+
+#  lists, dictionaries, and sets are not
+
+S = 'shrubbery'
+L = list(S)
+print(L)
+
+print(L[1])
+
+L[1] = 'c'
+
+print(L)
+
+print(''.join(L))
+
+
+print()
+
+B = bytearray(b'spam')
+B.extend(b'eggs')
+print(B)
+
+S = 'Spam'
+print(S)
