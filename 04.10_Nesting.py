440fa4cb3fea75f2197fabfef24efe954a5b076b
Nathan Worden <worden.nathan@gmail.com>
2021-05-26 18:09:38 -0700

    nesting

---
+++ 04.10_Nesting.py
@@ -0,0 +1,39 @@
+# Python's core data types support arbitrary nesting
+
+M = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+# print(M)
+# print(M[1])
+print(M[1][2])
+
+#  List comprehension expression
+
+col2 = [row[1] for row in M]
+print(col2)
+print()
+print(M)
+
+# List comprehensions derive from set notation; they are a way to build a new list by running an expression on each item in asequence, one at a time.
+
+print()
+col2_plus_one = [row[1] + 1 for row in M]
+print(col2_plus_one)
+
+print()
+col2_conditional = [row[1] for row in M if row[1] % 2 == 0]
+print(col2_conditional)
+
+print()
+diag = [M[i][i] for i in [0, 1, 2]]
+print(diag)
+
+
+print()
+doubles = [c * 2 for c in 'spam']
+print(doubles)
+
+print(list(range(10)))
+print()
+print(list(range(-6, 7, 2)))
