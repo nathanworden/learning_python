380e38b55ecec579b8878863a6c24dad244f5b06
Nathan Worden <worden.nathan@gmail.com>
2021-06-01 22:01:40 -0700

    truncation

--- 04.23_expression_operators.py
+++ 04.23_expression_operators.py
@@ -16,3 +16,17 @@ print(col2)
 
 tuple = (5, 6)
 print(tuple)
+
+print(40 + 3.14)
+
+print(int(3.1415))
+
+a = 3
+b = 4
+print(a + 1, a - 1)
+print(b * 3, b / 2)
+print(a % 2, b ** 2) # 1, 16
+print(2 + 4, 2.0 ** b)  # 6, 16
+print(b / 2 + a) # 5.0
+print(b / (2.0 + a))
+print(b / (2.0 + a))
---
+++ 04.24._numbers.py
@@ -0,0 +1,25 @@
+num = 1 / 3.0
+
+print('%e' % num)
+
+print('%4.2f' % num)
+
+print(repr('spam'))
+print(str('spam'))
+
+print(3/2)
+print(3//2)
+print(3//2.8)
+print(10/4)
+
+import math
+print(math.floor(2.5))
+print(math.floor(-2.5))
+print(math.trunc(2.5))
+print(math.trunc(-2.5))
+
+print()
+
+print(5 / -2)
+print(5 // -2)
+print(math.trunc(5 / -2))
