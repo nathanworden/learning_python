acdf066088d723a653c90729057ac8a6cc4cb9a8
Nathan Worden <worden.nathan@gmail.com>
2021-06-02 18:32:46 -0700

    bitwise operations

--- 04.25_truncation.py
+++ 04.25_truncation.py
@@ -4,3 +4,14 @@ print(9 / 3)
 print(2 ** 200)
 print(0o1)
 print(bin(64))
+
+print(eval('0o100'))
+print(eval('0x40'))
+
+print('{0:o}, {1:x}, {2:b}'.format(64, 64, 64))
+
+print('%o, %x, %x, %X' % (64, 64, 255, 255))
+
+
+X = 0xFFFFFFFFFFFFF
+print(X)
---
+++ 04.26_bitwise_operations.py
@@ -0,0 +1,1 @@
+# Besides the normal numeric operations like addition subtraction and so on, Python supports most of the numeric expressions available in the C language. This includes operators taht treat integers as strings of binary bits, and can come in handy if your Python code must deal with things like network packets, serial ports, or packed binary data produced by a C program.
