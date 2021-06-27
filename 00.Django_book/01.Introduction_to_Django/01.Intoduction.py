e27a254dfd5a23228d67e96a853e0454fa88fb0d
Nathan Worden <worden.nathan@gmail.com>
2021-06-26 17:39:09 -0700

    django intro' git satus

---
+++ 00.Django_book/01.Introduction_to_Django/01.Intoduction.py
@@ -0,0 +1,3 @@
+## Introduction
+
+## In the early days, Web developer wrote every page by hand. Udating a Web site meant editing HTML; a "redesign" invoved redoing every single page, one at a time.
